846d7f00845a38e1d03c89c4b086ab51020c2405
tsuzukick <33139194+tsuzukick@users.noreply.github.com>
2026-01-16 08:12:23 +0000

    test

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -4,6 +4,7 @@
 #from calendar import month
 #from sys import prefix
 import pandas as pd
+import click
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
 
@@ -32,18 +33,17 @@ parse_dates = [
 ]
 
 
-def run():
-    pg_user = 'root'
-    pg_password  = 'root'
-    pg_host = 'localhost'
-    pg_port = 5432
-    pg_database = 'ny_taxi'
-    year = 2021
-    month = 1
-
-    target_table = 'yellow_taxi_data'
-
-    chunksize = 100000
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL username')
+@click.option('--pg-password', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-database', default='ny_taxi', help='PostgreSQL database')
+@click.option('--year', default=2021, type=int, help='Year of data')
+@click.option('--month', default=1, type=int, help='Month of data')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+@click.option('--chunksizes', default=100000, type=int, help='Chunk size for reading CSV')
+def run(pg_user, pg_password, pg_host, pg_port, pg_database, year, month, target_table, chunksize):
 
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
     url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
